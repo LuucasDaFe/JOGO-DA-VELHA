07571c5219b244436bc295165070ac41c4cb40ff
Lucas Abraao <Labraao957@gmail.com>
2023-08-27 00:16:26 -0300

    Jogo da #

---
+++ script.js
@@ -0,0 +1,94 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const winningMessage = document.querySelector("[data-winning-message]");
+const restartButton = document.querySelector("[data-restart-Button]");
+
+let isCircleTurn;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const starGame = () => {
+  for (const cell of cellElements) {
+    cell.classList.remove("circle");
+    cell.classList.remove("x");
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+
+  isCircleTurn = false;
+
+  board.classList.add("x");
+  winningMessage.classList.remove("show-winning-message");
+};
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerHTML = "Empate";
+  } else {
+    winningMessageTextElement.innerHTML = isCircleTurn
+      ? "O Venceu"
+      : "X Venceu";
+  }
+
+  winningMessage.classList.add("show-winning-message");
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+  board.classList.remove("circle");
+  board.classList.remove("x");
+
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+const handleClick = (e) => {
+  // Colocar a marca (X ou Circulo)
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "circle" : "x";
+
+  placeMark(cell, classToAdd);
+
+  // Verificar por vitória
+  const isWin = checkForWin(classToAdd);
+  if (isWin) {
+    endGame(false);
+  }
+
+  // Verificar por empate
+
+  // Mudar síbolo
+  swapTurns();
+};
+
+starGame();
+
+restartButton.addEventListener("click", starGame);
